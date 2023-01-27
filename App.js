e8721d918bc4cc0d8ffb53bfb9d779874072fc55
Vaidik Singh Nirwan <vaidiksinghnirwan.com>
2023-01-28 00:59:49 +0530

    Lesson 1: react hooks

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
+  const getName = (first,second,third) => {
+    return first + ', ' + second + ', ' + third;
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Text>We are {getName('vaidik','sam','adam')}</Text>
       <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
     </View>
   );
 }
